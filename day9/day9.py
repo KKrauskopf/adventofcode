efbcedd6da41dcb30583c61b4d4e3c805acdea29
Kevin Krauskopf <Kevin.Krauskopf@lidl.com>
2021-12-09 10:01:42 +0100

    tracking exec time

---
+++ day9/day9.py
@@ -0,0 +1,12 @@
+import time
+
+lines = []
+
+with open('/Users/krauskopf/projects/adventofcode/day8/input.txt') as f:
+    for line in f.readlines():
+        lines.append(list(line))
+
+def solve1(lines):
+    print(lines)
+
+solve1(input)
