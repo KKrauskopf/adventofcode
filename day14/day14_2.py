fc8c8cc6ab52f7f71ffd8dd86a697c1638c7c7b8
Kevin Krauskopf <Kevin.Krauskopf@lidl.com>
2021-12-15 11:23:18 +0100

    finally solve day 14

--- day14/day14_2.py
+++ day14/day14_2.py
@@ -13,21 +13,22 @@ with open('/Users/krauskopf/projects/adventofcode/day14/input.txt') as f:
 
 def solve2(start):
     pair_counts = defaultdict(int)
+    letter_counts = defaultdict(int)
     for i in range(0,len(start)-1):
         pair_counts[start[i] + start[i+1]] += 1
-    for _ in range(0,40):
+    for char in start:
+        letter_counts[char] += 1
+    for i in range(0,40):
         new_pair_counts = defaultdict(int)
-        letters = defaultdict(int)
         for key, value in pair_counts.items():
             child1, child2 = list(mappings[key])
             new_pair_counts[child1] += value
             new_pair_counts[child2] += value
-            for char in list(child1[:-1]+child2[1:]):
-                letters[char] += value
+            letter_counts[child1[1:]] += value
         pair_counts = new_pair_counts
 
     #letter_values = [value/2 if value %2 == 0 else (value-1)/2 for _ ,value in letters.items()]
-    letter_values = [value for _, value in letters.items()]
+    letter_values = [value for value in letter_counts.values()]
     maxv = max(letter_values)
     minv = min(letter_values)
     print(maxv - minv)
