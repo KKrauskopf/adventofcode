3d8bf0c6f35dafb5eaead637957b0d48b17cbbb0
Kevin Krauskopf <Kevin.Krauskopf@lidl.com>
2022-12-06 10:00:19 +0100

    dumb but working day6

--- 2022/day6/solution.py
+++ 2022/day6/solution.py
@@ -1,13 +1,23 @@
 input = ""
 
-with open('/Users/krauskopf/projects/adventofcode/2022/day5/input.txt') as f:
-    for index, line in f.readlines():
+with open('/Users/krauskopf/projects/adventofcode/2022/day6/input.txt') as f:
+    for line in f.readlines():
         input = line
 
 def solve1():
     chars = list(input)
-    for i in range(3, len(chars)):
-        check_range = set(chars[i],chars[i-1],chars[i-2],chars[i-3])
-        print(len(check_range))
+    for i in range(4, len(chars)):
+        check_range = set([chars[i],chars[i-1],chars[i-2],chars[i-3]])
+        if len(check_range) == 4:
+            print(i+1)
+            break
 
-solve1()
+def solve2():
+    chars = list(input)
+    for i in range(14, len(chars)):
+        check_range = set([chars[i],chars[i-1],chars[i-2],chars[i-3],chars[i-4],chars[i-5],chars[i-6],chars[i-7],chars[i-8],chars[i-9],chars[i-10],chars[i-11],chars[i-12],chars[i-13]])
+        if len(check_range) == 14:
+            print(i+1)
+            break
+
+solve2()
